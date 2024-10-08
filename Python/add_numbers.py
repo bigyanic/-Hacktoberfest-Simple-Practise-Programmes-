e40a9b68d6f0d1fa5269c8429d5328643f19bd66
duajessani <dua.jessani@gmail.com>
2024-10-08 11:37:08 +0500

    sum of two numbers

--- Python/add_numbers.py
+++ Python/add_numbers.py
@@ -1,9 +1,9 @@
-def sum():
-    num1 = int(input("Enter First Number"))
-    num2 = int(input("Enter Second Number"))
-    sum = num1 + num2
-    return sum
+def sumnumbers()
+    number_1 = int(input("Enter 1st number: "))
+    number_2 = int(input("Enter 2nd number: "))
+    sum = number_1 + number_2
+    returm sum
 
-addition = sum()
-print(addition)
 
+sum=sumnumbers()
+print(sum)
